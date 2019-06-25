73ea8585b5579fb183b642af338675653f0ecc30
Josiah Morgan <Josiah.Morgan@nov.com>
2019-06-24 21:40:35 -0500

    Add project files.

---
+++ ImageCropTool/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace ImageCropTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.filepathLabel = new System.Windows.Forms.Label();
+            this.SelectImageButton = new System.Windows.Forms.Button();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Titles = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Box1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Box2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Box3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Box4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Box5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Box6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Box7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Box8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Box9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Box10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // filepathLabel
+            // 
+            this.filepathLabel.AutoSize = true;
+            this.filepathLabel.Location = new System.Drawing.Point(179, 173);
+            this.filepathLabel.Name = "filepathLabel";
+            this.filepathLabel.Size = new System.Drawing.Size(85, 13);
+            this.filepathLabel.TabIndex = 8;
+            this.filepathLabel.Text = "No File Selected";
+            // 
+            // SelectImageButton
+            // 
+            this.SelectImageButton.Location = new System.Drawing.Point(82, 168);
+            this.SelectImageButton.Name = "SelectImageButton";
+            this.SelectImageButton.Size = new System.Drawing.Size(90, 23);
+            this.SelectImageButton.TabIndex = 9;
+            this.SelectImageButton.Text = "Select Image";
+            this.SelectImageButton.UseVisualStyleBackColor = true;
+            this.SelectImageButton.Click += new System.EventHandler(this.SelectImageButton_Click);
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.SaveButton.Location = new System.Drawing.Point(1100, 545);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(75, 23);
+            this.SaveButton.TabIndex = 10;
+            this.SaveButton.Text = "Save Image";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Titles,
+            this.Box1,
+            this.Box2,
+            this.Box3,
+            this.Box4,
+            this.Box5,
+            this.Box6,
+            this.Box7,
+            this.Box8,
+            this.Box9,
+            this.Box10});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 215);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1145, 225);
+            this.dataGridView1.TabIndex = 11;
+            //this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Titles
+            // 
+            this.Titles.HeaderText = "Titles";
+            this.Titles.Name = "Titles";
+            this.Titles.ReadOnly = true;
+            // 
+            // Box1
+            // 
+            this.Box1.HeaderText = "Box1";
+            this.Box1.Name = "Box1";
+            // 
+            // Box2
+            // 
+            this.Box2.HeaderText = "Box2";
+            this.Box2.Name = "Box2";
+            // 
+            // Box3
+            // 
+            this.Box3.HeaderText = "Box3";
+            this.Box3.Name = "Box3";
+            // 
+            // Box4
+            // 
+            this.Box4.HeaderText = "Box4";
+            this.Box4.Name = "Box4";
+            // 
+            // Box5
+            // 
+            this.Box5.HeaderText = "Box5";
+            this.Box5.Name = "Box5";
+            // 
+            // Box6
+            // 
+            this.Box6.HeaderText = "Box6";
+            this.Box6.Name = "Box6";
+            // 
+            // Box7
+            // 
+            this.Box7.HeaderText = "Box7";
+            this.Box7.Name = "Box7";
+            // 
+            // Box8
+            // 
+            this.Box8.HeaderText = "Box8";
+            this.Box8.Name = "Box8";
+            // 
+            // Box9
+            // 
+            this.Box9.HeaderText = "Box9";
+            this.Box9.Name = "Box9";
+            // 
+            // Box10
+            // 
+            this.Box10.HeaderText = "Box10";
+            this.Box10.Name = "Box10";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1187, 580);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.SelectImageButton);
+            this.Controls.Add(this.filepathLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label filepathLabel;
+        private System.Windows.Forms.Button SelectImageButton;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Titles;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Box1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Box2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Box3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Box4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Box5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Box6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Box7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Box8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Box9;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Box10;
+    }
+}
+
---
+++ ImageCropTool/Form1.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageCropTool
+{
+    public partial class Form1 : Form
+    {
+        private string filepath = "";
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridView1.KeyUp += dataGridView1_KeyUp;
+        }
+
+        private void SelectImageButton_Click(object sender, EventArgs e)
+        {
+            Stream myStream = null;
+            OpenFileDialog theDialog = new OpenFileDialog();
+            theDialog.Title = "Open jpg File";
+            theDialog.Filter = "JPG files|*.jpg";
+            theDialog.InitialDirectory = @"C:\";
+            if (theDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((myStream = theDialog.OpenFile()) != null)
+                    {
+                        using (myStream)
+                        {
+                            // Insert code to read the stream here.
+                        }
+                    }
+                    filepath = theDialog.FileName;
+                    filepathLabel.Text = filepath;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "JPeg Image|*.jpg|Bitmap Image|*.bmp|Gif Image|*.gif";
+            saveFileDialog1.Title = "Save an Image File";
+            saveFileDialog1.ShowDialog();
+
+            // If the file name is not an empty string open it for saving.  
+            if (saveFileDialog1.FileName != "")
+            {
+
+                if (filepath == "")
+                {
+                    MessageBox.Show("No File Selected");
+                }
+                else if (System.IO.File.Exists(filepath) == false)
+                {
+                    MessageBox.Show("File Doesn't Exist");
+                }
+                else
+                {
+                    Pen blackPen = new Pen(Color.Black, 2);
+                    Brush brush = new SolidBrush(Color.FromArgb(125, 20, 20, 20));
+                    Image OriginalImage = Image.FromFile(filepath);
+                    Image NewImage = Image.FromFile(filepath);
+                    Graphics originalGraphics = Graphics.FromImage(NewImage);
+                    for (int colNum =1; colNum<11; colNum++)
+                    {
+                        if ((string)dataGridView1.Rows[0].Cells[colNum].Value != "" && dataGridView1.Rows[0].Cells[colNum].Value != null)
+                        {
+                            Rectangle CropRect = new Rectangle(Convert.ToInt32(dataGridView1.Rows[2].Cells[colNum].Value), Convert.ToInt32(dataGridView1.Rows[3].Cells[colNum].Value), Convert.ToInt32(dataGridView1.Rows[0].Cells[colNum].Value), Convert.ToInt32(dataGridView1.Rows[1].Cells[colNum].Value));
+                            Rectangle CropRectZero = new Rectangle(0, 0, CropRect.Width, CropRect.Height);
+                            Bitmap CropImage = new Bitmap(CropRect.Width, CropRect.Height);
+                            if ((string)dataGridView1.Rows[5].Cells[colNum].Value != "" && dataGridView1.Rows[5].Cells[colNum].Value != null)
+                            {
+                                using (Graphics cropGraphics = Graphics.FromImage(CropImage))
+                                {
+                                    cropGraphics.DrawImage(OriginalImage, CropRectZero, CropRect, GraphicsUnit.Pixel);
+                                    //System.IO.Path.GetDirectoryName(saveFileDialog1.FileName);
+                                    CropImage.Save(System.IO.Path.GetDirectoryName(saveFileDialog1.FileName) + "\\" + (string)dataGridView1.Rows[5].Cells[colNum].Value + ".jpg");
+                                }
+                            }
+                            if ((string)dataGridView1.Rows[4].Cells[colNum].Value == "" || dataGridView1.Rows[4].Cells[colNum].Value == null) //border no fill
+                            {
+                                originalGraphics.DrawRectangle(blackPen, CropRect);
+                            }
+                            else
+                            {
+                                originalGraphics.FillRectangle(brush, CropRect);
+                            }
+                        }
+                    }
+                    //MessageBox.Show(System.IO.Path.GetDirectoryName(saveFileDialog1.FileName));
+                    NewImage.Save(saveFileDialog1.FileName);
+                    NewImage.Dispose();
+                    OriginalImage.Dispose();
+                }
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Add();
+            dataGridView1.Rows.Add();
+            dataGridView1.Rows.Add();
+            dataGridView1.Rows.Add();
+            dataGridView1.Rows.Add();
+            dataGridView1.Rows.Add();
+            dataGridView1.Rows[0].Cells[0].Value = "Width";
+            dataGridView1.Rows[1].Cells[0].Value = "Height";
+            dataGridView1.Rows[2].Cells[0].Value = "X";
+            dataGridView1.Rows[3].Cells[0].Value = "Y";
+            dataGridView1.Rows[4].Cells[0].Value = "fill";
+            dataGridView1.Rows[5].Cells[0].Value = "saveName";
+
+
+        }
+
+        private void dataGridView1_KeyUp(object sender, KeyEventArgs e)
+
+        {
+
+            //if user clicked Shift+Ins or Ctrl+V (paste from clipboard)
+
+            if ((e.KeyCode == Keys.Delete))
+            {
+                int firstRow = 999; //dataGridView1.SelectedCells[0].RowIndex;
+                int firstCol = 9999;// dataGridView1.SelectedCells[0].ColumnIndex;
+                int lastRow = -1;// dataGridView1.SelectedCells[dataGridView1.SelectedCells.Count-1].RowIndex;
+                int lastCol = -1;// dataGridView1.SelectedCells[dataGridView1.SelectedCells.Count - 1].ColumnIndex;
+                foreach(DataGridViewCell cell in dataGridView1.SelectedCells)
+                {
+                    firstRow = Math.Min(firstRow, cell.RowIndex);
+                    firstCol = Math.Min(firstCol, cell.ColumnIndex);
+                    lastRow = Math.Max(lastRow, cell.RowIndex);
+                    lastCol = Math.Max(lastCol, cell.ColumnIndex);
+                }
+                for(int r = firstRow; r<= lastRow; r++)
+                {
+                    for (int c = firstCol; c <= lastCol; c++)
+                    {
+                        dataGridView1.Rows[r].Cells[c].Value = "";
+                    }
+                }
+            }
+                if ((e.Shift && e.KeyCode == Keys.Insert) || (e.Control && e.KeyCode == Keys.V))
+
+            {
+
+                char[] rowSplitter = { '\r', '\n' };
+
+                char[] columnSplitter = { '\t' };
+
+                //get the text from clipboard
+
+                IDataObject dataInClipboard = Clipboard.GetDataObject();
+
+                string stringInClipboard = (string)dataInClipboard.GetData(DataFormats.Text);
+
+                //split it into lines
+
+                string[] rowsInClipboard = stringInClipboard.Split(rowSplitter, StringSplitOptions.RemoveEmptyEntries);
+
+                //get the row and column of selected cell in grid
+
+                int r = 9999;// dataGridView1.SelectedCells[0].RowIndex;
+
+                int c = 9999;// dataGridView1.SelectedCells[0].ColumnIndex;
+
+
+
+                foreach (DataGridViewCell cell in dataGridView1.SelectedCells)
+                {
+                    r = Math.Min(r, cell.RowIndex);
+                    c = Math.Min(c, cell.ColumnIndex);
+                }
+
+                //add rows into grid to fit clipboard lines
+
+                if (dataGridView1.Rows.Count < (r + rowsInClipboard.Length))
+
+                {
+
+                    dataGridView1.Rows.Add(r + rowsInClipboard.Length - dataGridView1.Rows.Count);
+
+                }
+
+                // loop through the lines, split them into cells and place the values in the corresponding cell.
+
+                for (int iRow = 0; iRow < rowsInClipboard.Length; iRow++)
+
+                {
+
+                    //split row into cell values
+
+                    string[] valuesInRow = rowsInClipboard[iRow].Split(columnSplitter);
+
+                    //cycle through cell values
+
+                    for (int iCol = 0; iCol < valuesInRow.Length; iCol++)
+
+                    {
+
+                        //assign cell value, only if it within columns of the grid
+
+                        if (dataGridView1.ColumnCount - 1 >= c + iCol)
+
+                        {
+
+                            dataGridView1.Rows[r + iRow].Cells[c + iCol].Value = valuesInRow[iCol];
+
+                        }
+
+                    }
+
+                }
+
+            }
+
+        }
+    }
+}
